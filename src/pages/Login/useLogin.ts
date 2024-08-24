e4c42b968df5e786507c6fc2f1f87efb0fe0b32d
elvesbd <elvesbd@yahoo.com>
2024-08-24 10:35:13 -0300

    chore: adding navigation fot todo pages and applying notification context

--- src/pages/Login/useLogin.ts
+++ src/pages/Login/useLogin.ts
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import useAuth from "../../hooks/auth/useAuth";
-import useNotification from "../../hooks/notifications/useNotification";
 import AuthService from "../../services/auth/AuthService";
+import useNotification from "../../hooks/notifications/useNotification";
 
 export function useLogin() {
-  const { setAuthData } = useAuth();
-  const { notifySuccess, notifyError } = useNotification();
+  const navigate = useNavigate();
+  const { setToken } = useAuth();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const { notifySuccess, notifyError } = useNotification();
 
   const handleClearForm = () => {
     setEmail("");
@@ -34,12 +37,16 @@ export function useLogin() {
     setError(null);
 
     try {
-      const user = await AuthService.login(email, password);
-      setAuthData(user);
+      //const { accessToken } = await AuthService.login(email, password);
+      const accessToken = "123";
+      setToken(accessToken);
       notifySuccess("Seja bem vindo ao Todo App!");
+      navigate("/todos");
       handleClearForm();
     } catch (err) {
+      console.log(err);
       notifyError("Não foi possível efetuar o login!");
+      navigate("/");
     } finally {
       setLoading(false);
     }
